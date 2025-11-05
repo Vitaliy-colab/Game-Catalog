fda283131e604301eafb232355111bba40b3d769
Vitaliy-colab <vitaliyzaritskuy@gmail.com>
2025-11-05 12:02:53 +0200

    Initial Commit

---
+++ Game Catalog/Game_CatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_CatalogApp.swift
+//  Game Catalog
+//
+//  Created by mac on 05.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct Game_CatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Game CatalogTests/Game_CatalogTests.swift
@@ -0,0 +1,17 @@
+//
+//  Game_CatalogTests.swift
+//  Game CatalogTests
+//
+//  Created by mac on 05.11.2025.
+//
+
+import Testing
+@testable import Game_Catalog
+
+struct Game_CatalogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
